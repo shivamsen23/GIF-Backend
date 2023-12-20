55cd4e7c3dfd701744945b9fa6363606cd6c3842
shivamsen23 <97625468+shivamsen23@users.noreply.github.com>
2023-12-20 11:37:30 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const secretKey = crypto.randomBytes(32).toString('hex');
+console.log('Generated Secret Key:', secretKey);
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+//mongodb Connection Database
+const uri = "mongodb+srv://senshivam838:rimpasen@cluster0.50bjghz.mongodb.net/";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB', err));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  phone: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Registration endpoint
+app.post('/api/register', async (req, res) => {
+  try {
+    const { name, email, phone, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, phone, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+// Login endpoint
+app.post('/api/login', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      
+      const user = await User.findOne({ email });
+  
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+  
+      const token = jwt.sign({ userId: user._id }, secretKey , { expiresIn: '1h' });
+  
+      res.json({ token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  // Logout endpoint
+app.post('/api/logout', (req, res) => {
+    res.json({ message: 'Logout successful' });
+  });
+  
+  
+app.listen(PORT, () => {
+  console.log('Server is listening on port', PORT);
+});
