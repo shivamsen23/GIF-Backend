2ed71c4dd797b0bfe64219e30f9282419093d8a5
shivamsen23 <97625468+shivamsen23@users.noreply.github.com>
2023-12-20 16:53:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use('/static', express.static(path.join(__dirname, '../src/app')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/app/page1'));
+  res.sendFile(path.join(__dirname, '../src/app/page1.tsx'));
 });
 
 
